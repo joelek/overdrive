7f2939d88acc575ca157d5bddede66733480eda7
Joel Ek <contact@joelek.se>
2024-09-18 07:55:23 +0200

    Clean up idiv module.

--- source/idiv.cpp
+++ source/idiv.cpp
@@ -1,18 +1,24 @@
 #include "idiv.h"
 
 namespace idiv {
-	auto floor(int n, int d)
-	-> int {
-		return n / d - (n % d != 0 && (n ^ d) < 0);
-	}
-
-	auto ceil(int n, int d)
-	-> int {
+	auto ceil(
+		int n,
+		int d
+	) -> int {
 		return n / d + (n % d != 0 && (n ^ d) > 0);
 	}
 
-	auto round(int n, int d)
-	-> int {
+	auto floor(
+		int n,
+		int d
+	) -> int {
+		return n / d - (n % d != 0 && (n ^ d) < 0);
+	}
+
+	auto round(
+		int n,
+		int d
+	) -> int {
 		return (n ^ d) < 0 ? (n - d / 2) / d : (n + d / 2) / d;
 	}
 }
--- source/idiv.h
+++ source/idiv.h
@@ -1,9 +1,9 @@
 #pragma once
 
 namespace idiv {
-	auto floor(int n, int d) -> int;
-
 	auto ceil(int n, int d) -> int;
+
+	auto floor(int n, int d) -> int;
 
 	auto round(int n, int d) -> int;
 }
