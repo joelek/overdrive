ff70d15c7c892de529106dee8142b5634b931e91
Joel Ek <contact@joelek.se>
2024-10-03 17:50:03 +0200

    Add enum helper for TrackType enum.

--- source/lib/enums.cpp
+++ source/lib/enums.cpp
@@ -6,14 +6,33 @@
 namespace overdrive {
 namespace enums {
 	auto SessionType(
-		cdb::SessionType session_type
+		cdb::SessionType value
 	) -> const std::string& {
 		static const auto names = std::map<cdb::SessionType, std::string>({
 			{ cdb::SessionType::CDDA_OR_CDROM, "CDDA_OR_CDROM" },
 			{ cdb::SessionType::CDI, "CDI" },
 			{ cdb::SessionType::CDXA_OR_DDCD, "CDXA_OR_DDCD" }
 		});
-		auto iterator = names.find(session_type);
+		auto iterator = names.find(value);
+		if (iterator == names.end()) {
+			throw exceptions::MissingValueException("name");
+		}
+		return iterator->second;
+	}
+
+	auto TrackType(
+		drive::TrackType value
+	) -> const std::string& {
+		static const auto names = std::map<drive::TrackType, std::string>({
+			{ drive::TrackType::AUDIO_2_CHANNELS, "AUDIO_2_CHANNELS" },
+			{ drive::TrackType::AUDIO_4_CHANNELS, "AUDIO_4_CHANNELS" },
+			{ drive::TrackType::DATA_MODE0, "DATA_MODE0" },
+			{ drive::TrackType::DATA_MODE1, "DATA_MODE1" },
+			{ drive::TrackType::DATA_MODE2, "DATA_MODE2" },
+			{ drive::TrackType::DATA_MODE2_FORM1, "DATA_MODE2_FORM1" },
+			{ drive::TrackType::DATA_MODE2_FORM2, "DATA_MODE2_FORM2" },
+		});
+		auto iterator = names.find(value);
 		if (iterator == names.end()) {
 			throw exceptions::MissingValueException("name");
 		}
--- source/lib/enums.h
+++ source/lib/enums.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include "cdb.h"
+#include "drive.h"
 #include "type.h"
 
 namespace overdrive {
@@ -9,7 +10,11 @@ namespace enums {
 	using namespace type;
 
 	auto SessionType(
-		cdb::SessionType session_type
+		cdb::SessionType value
+	) -> const std::string&;
+
+	auto TrackType(
+		drive::TrackType value
 	) -> const std::string&;
 }
 }
