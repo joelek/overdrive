3ac442a9961599230517b4e0b87823cdc5798e9d
Joel Ek <contact@joelek.se>
2024-10-08 19:04:50 +0200

    Use relative std namespace in memory module.

--- source/lib/memory.cpp
+++ source/lib/memory.cpp
@@ -10,7 +10,7 @@ namespace memory {
 		byte_t value
 	) -> bool_t {
 		auto buffer = reinterpret_cast<byte_t*>(pointer);
-		return (buffer[0] == value) && ::std::memcmp(buffer, buffer + 1, size - 1) == 0;
+		return (buffer[0] == value) && std::memcmp(buffer, buffer + 1, size - 1) == 0;
 	}
 }
 }
