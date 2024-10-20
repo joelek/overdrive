33e698e005074dbd8df8c6d15e652aa4664f4564
Joel Ek <contact@joelek.se>
2024-10-20 11:12:48 +0200

    Save work on ODI command.

--- source/cli/commands/odi.cpp
+++ source/cli/commands/odi.cpp
@@ -24,10 +24,43 @@ namespace commands {
 			}
 		}
 
-		auto assert_image_compatibility(
-			const std::vector<disc::TrackInfo>& tracks
-		) -> void {
-			(void)tracks;
+		auto save_sector_range(
+			const drive::Drive& drive,
+			si_t first_sector,
+			si_t last_sector,
+			const ODIptions& options,
+			std::FILE* handle,
+			const std::string& path
+		) -> std::vector<odi::SectorTableEntry> {
+			auto extracted_sectors_vector = copier::read_absolute_sector_range(
+				drive,
+				first_sector,
+				last_sector,
+				options.min_data_passes,
+				options.max_data_passes,
+				options.max_data_retries,
+				options.min_data_copies,
+				options.max_data_copies
+			);
+			auto bad_sector_indices = copier::get_bad_sector_indices(extracted_sectors_vector, first_sector);
+			fprintf(stderr, "%s\n", std::format("Sector range between {} and {} has {} bad sectors!", first_sector, last_sector, bad_sector_indices.size()).c_str());
+			auto sector_table_entries = std::vector<odi::SectorTableEntry>(last_sector - first_sector);
+			for (auto sector_index = size_t(0); sector_index < extracted_sectors_vector.size(); sector_index += 1) {
+				auto& extracted_sectors = extracted_sectors_vector.at(sector_index);
+				auto& extracted_sector = extracted_sectors.at(0);
+				auto& sector_table_entry = sector_table_entries.at(sector_index);
+				sector_table_entry.compressed_data_absolute_offset = std::ftell(handle);
+				sector_table_entry.compressed_byte_count = sizeof(odi::UncompressedSector);
+				sector_table_entry.compression_method = odi::CompressionMethod::NONE;
+				sector_table_entry.readability = extracted_sector.counter == 0 ? odi::Readability::UNREADABLE : odi::Readability::READABLE;
+				if (std::fwrite(extracted_sector.sector_data, sizeof(extracted_sector.sector_data), 1, handle) != 1) {
+					OVERDRIVE_THROW(exceptions::IOWriteException(path));
+				}
+				if (std::fwrite(extracted_sector.subchannels_data, sizeof(extracted_sector.subchannels_data), 1, handle) != 1) {
+					OVERDRIVE_THROW(exceptions::IOWriteException(path));
+				}
+			}
+			return sector_table_entries;
 		}
 
 		auto write_odi(
@@ -35,7 +68,6 @@ namespace commands {
 			const disc::DiscInfo& disc,
 			const ODIptions& options
 		) -> void {
-			auto tracks = disc::get_disc_tracks(disc);
 			auto points = disc::get_disc_points(disc);
 			auto path = path::create_path(options.path)
 				.with_extension(".odi")
@@ -43,69 +75,77 @@ namespace commands {
 			auto handle = copier::open_handle(path);
 			try {
 				auto file_header = odi::FileHeader();
-				file_header.sector_table_header_absolute_offset = 0; // TODO
-				file_header.point_table_header_absolute_offset = 0; // TODO
+				file_header.sector_table_header_absolute_offset = 0;
+				file_header.point_table_header_absolute_offset = 0;
 				if (std::fwrite(&file_header, sizeof(file_header), 1, handle) != 1) {
 					OVERDRIVE_THROW(exceptions::IOWriteException(path));
 				}
-
-
 				auto sector_table_entries = std::vector<odi::SectorTableEntry>();
-				sector_table_entries.reserve(0); // TODO
-				for (auto track_index = size_t(0); track_index < tracks.size(); track_index += 1) {
-					auto& track = tracks.at(track_index);
-					auto extracted_sectors_vector = copier::read_track(drive, track, options);
-					auto bad_sector_indices = copier::get_bad_sector_indices(extracted_sectors_vector, track.first_sector_absolute);
-					copier::log_bad_sector_indices(drive, track, bad_sector_indices);
-					for (auto sector_index = size_t(0); sector_index < extracted_sectors_vector.size(); sector_index += 1) {
-						auto& extracted_sectors = extracted_sectors_vector.at(sector_index);
-						auto& extracted_sector = extracted_sectors.at(0);
-						auto& sector_table_entry = sector_table_entries.at(sector_index + track.first_sector_absolute);
-						sector_table_entry.compressed_data_absolute_offset = std::ftell(handle);
-						if (std::fwrite(extracted_sector.sector_data, sizeof(extracted_sector.sector_data), 1, handle) != 1) {
-							OVERDRIVE_THROW(exceptions::IOWriteException(path));
+				auto absolute_sector_offset = 0 - si_t(disc.sessions.front().lead_in_length_sectors);
+				for (auto session_index = size_t(0); session_index < disc.sessions.size(); session_index += 1) {
+					auto& session = disc.sessions.at(session_index);
+					auto lead_in_sector_table_entries = save_sector_range(drive, absolute_sector_offset, absolute_sector_offset + session.lead_in_length_sectors, options, handle, path);
+					vector::append(sector_table_entries, lead_in_sector_table_entries);
+					absolute_sector_offset += session.lead_in_length_sectors;
+					auto pregap_sector_table_entries = save_sector_range(drive, absolute_sector_offset, absolute_sector_offset + session.pregap_sectors, options, handle, path);
+					vector::append(sector_table_entries, pregap_sector_table_entries);
+					absolute_sector_offset += session.pregap_sectors;
+					for (auto track_index = size_t(0); track_index < session.tracks.size(); track_index += 1) {
+						auto& track = session.tracks.at(track_index);
+						auto extracted_sectors_vector = copier::read_track(drive, track, options);
+						auto bad_sector_indices = copier::get_bad_sector_indices(extracted_sectors_vector, track.first_sector_absolute);
+						copier::log_bad_sector_indices(drive, track, bad_sector_indices);
+						auto track_sector_table_entries = std::vector<odi::SectorTableEntry>(track.length_sectors);
+						for (auto sector_index = size_t(0); sector_index < extracted_sectors_vector.size(); sector_index += 1) {
+							auto& extracted_sectors = extracted_sectors_vector.at(sector_index);
+							auto& extracted_sector = extracted_sectors.at(0);
+							auto& sector_table_entry = track_sector_table_entries.at(sector_index);
+							sector_table_entry.compressed_data_absolute_offset = std::ftell(handle);
+							sector_table_entry.compressed_byte_count = sizeof(odi::UncompressedSector);
+							sector_table_entry.compression_method = odi::CompressionMethod::NONE;
+							sector_table_entry.readability = extracted_sector.counter == 0 ? odi::Readability::UNREADABLE : odi::Readability::READABLE;
+							if (std::fwrite(extracted_sector.sector_data, sizeof(extracted_sector.sector_data), 1, handle) != 1) {
+								OVERDRIVE_THROW(exceptions::IOWriteException(path));
+							}
+							if (std::fwrite(extracted_sector.subchannels_data, sizeof(extracted_sector.subchannels_data), 1, handle) != 1) {
+								OVERDRIVE_THROW(exceptions::IOWriteException(path));
+							}
 						}
-						if (std::fwrite(extracted_sector.subchannels_data, sizeof(extracted_sector.subchannels_data), 1, handle) != 1) {
-							OVERDRIVE_THROW(exceptions::IOWriteException(path));
-						}
-
+						vector::append(sector_table_entries, track_sector_table_entries);
+						absolute_sector_offset += track.length_sectors;
 					}
+					auto lead_out_sector_table_entries = save_sector_range(drive, absolute_sector_offset, absolute_sector_offset + session.lead_out_length_sectors, options, handle, path);
+					vector::append(sector_table_entries, lead_out_sector_table_entries);
+					absolute_sector_offset += session.lead_out_length_sectors;
 				}
-
-
-				file_header.sector_table_header_absolute_offset = std::ftell(handle);
+				auto point_table_entries = std::vector<odi::PointTableEntry>(points.size());
+				for (auto point_index = size_t(0); point_index < points.size(); point_index += 1) {
+					auto& point = points.at(point_index);
+					auto point_table_entry = odi::PointTableEntry();
+					*reinterpret_cast<cdb::ReadTOCResponseFullTOCEntry*>(&point_table_entry.entry) = point.entry;
+				}
 				auto sector_table_header = odi::SectorTableHeader();
-				sector_table_header.entry_count = cd::RELATIVE_SECTOR_OFFSET + 0; // TODO
+				sector_table_header.entry_count = sector_table_entries.size();
 				sector_table_header.entry_length = sizeof(odi::SectorTableEntry);
+				file_header.sector_table_header_absolute_offset = std::ftell(handle);
 				if (std::fwrite(&sector_table_header, sizeof(sector_table_header), 1, handle) != 1) {
 					OVERDRIVE_THROW(exceptions::IOWriteException(path));
 				}
-				for (auto sector_index = size_t(0); sector_index < sector_table_header.entry_count; sector_index += 1) {
-					auto sector_table_entry = odi::SectorTableEntry();
-					sector_table_entry.compressed_data_absolute_offset = 0; // TODO
-					sector_table_entry.compressed_byte_count = sizeof(odi::UncompressedSector);
-					sector_table_entry.compression_method = odi::CompressionMethod::NONE;
-					sector_table_entry.readability = odi::Readability::UNREADABLE; // TODO
+				for (auto sector_table_index = size_t(0); sector_table_index < sector_table_header.entry_count; sector_table_index += 1) {
+					auto& sector_table_entry = sector_table_entries.at(sector_table_index);
 					if (std::fwrite(&sector_table_entry, sizeof(sector_table_entry), 1, handle) != 1) {
 						OVERDRIVE_THROW(exceptions::IOWriteException(path));
 					}
 				}
-
-
-
-
-
-				file_header.point_table_header_absolute_offset = std::ftell(handle);
 				auto point_table_header = odi::PointTableHeader();
-				point_table_header.entry_count = points.size();
+				point_table_header.entry_count = point_table_entries.size();
 				point_table_header.entry_length = sizeof(odi::PointTableEntry);
-				if (std::fwrite(&sector_table_header, sizeof(sector_table_header), 1, handle) != 1) {
+				file_header.point_table_header_absolute_offset = std::ftell(handle);
+				if (std::fwrite(&point_table_header, sizeof(point_table_header), 1, handle) != 1) {
 					OVERDRIVE_THROW(exceptions::IOWriteException(path));
 				}
-				for (auto point_index = size_t(0); point_index < point_table_header.entry_count; point_index += 1) {
-					auto& point = points.at(point_index);
-					auto point_table_entry = odi::PointTableEntry();
-					*reinterpret_cast<cdb::ReadTOCResponseFullTOCEntry*>(&point_table_entry.entry) = point.entry;
+				for (auto point_table_index = size_t(0); point_table_index < point_table_header.entry_count; point_table_index += 1) {
+					auto& point_table_entry = point_table_entries.at(point_table_index);
 					if (std::fwrite(&point_table_entry, sizeof(point_table_entry), 1, handle) != 1) {
 						OVERDRIVE_THROW(exceptions::IOWriteException(path));
 					}
