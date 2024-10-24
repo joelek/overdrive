778fa6380ee665476e8673d91cf30009cfa8d531
Joel Ek <contact@joelek.se>
2024-10-24 09:48:11 +0200

    Improve CDDA constants.

--- source/lib/cdda.h
+++ source/lib/cdda.h
@@ -8,31 +8,25 @@ namespace cdda {
 	using namespace shared;
 
 	const auto SECTOR_LENGTH = size_t(cd::SECTOR_LENGTH);
-	const auto STEREO_SAMPLE_LENGTH = size_t(4);
-	const auto STEREO_SAMPLES_PER_FRAME = size_t(6);
-	const auto STEREO_FRAME_LENGTH = size_t(STEREO_SAMPLES_PER_FRAME * STEREO_SAMPLE_LENGTH);
-	const auto STEREO_SECTOR_LENGTH = size_t(SECTOR_LENGTH);
-	const auto STEREO_FRAMES_PER_SECTOR = size_t(STEREO_SECTOR_LENGTH / STEREO_FRAME_LENGTH);
+	const auto FRAME_LENGTH = size_t(cd::PACKET_DATA_LENGTH);
+	const auto STEREO_CHANNELS = size_t(2);
+	const auto STEREO_SAMPLE_LENGTH = size_t(STEREO_CHANNELS * sizeof(si16_t));
+	const auto STEREO_SAMPLES_PER_SECTOR = size_t(SECTOR_LENGTH / STEREO_SAMPLE_LENGTH);
+	const auto STEREO_SAMPLES_PER_FRAME = size_t(FRAME_LENGTH / STEREO_SAMPLE_LENGTH);
 
 	#pragma pack(push, 1)
 
-	struct StereoFrame {
-		byte_t samples[STEREO_SAMPLES_PER_FRAME][STEREO_SAMPLE_LENGTH];
-	};
-
-	static_assert(sizeof(StereoFrame) == STEREO_FRAME_LENGTH);
-
 	struct StereoSector {
-		StereoFrame frames[STEREO_FRAMES_PER_SECTOR];
+		si16_t samples[STEREO_SAMPLES_PER_SECTOR][STEREO_CHANNELS];
 	};
 
-	static_assert(sizeof(StereoSector) == STEREO_SECTOR_LENGTH);
+	static_assert(sizeof(StereoSector) == 2352);
 
 	union Sector {
 		StereoSector stereo;
 	};
 
-	static_assert(sizeof(Sector) == SECTOR_LENGTH);
+	static_assert(sizeof(Sector) == 2352);
 
 	#pragma pack(pop)
 }
