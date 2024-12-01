2e75280ea7156d320a40f4e07e1b97f685fcd67c
Joel Ek <contact@joelek.se>
2024-12-01 08:06:02 +0100

    Set default value for audio-min-copies to 0.

--- source/lib/options.cpp
+++ source/lib/options.cpp
@@ -170,7 +170,7 @@ namespace options {
 			std::regex("^([0-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])$"),
 			"integer",
 			false,
-			std::optional<std::string>("1"),
+			std::optional<std::string>("0"),
 			1,
 			1,
 			[&](const std::vector<std::string>& matches) -> void {
